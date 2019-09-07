1c6ebcf37960223af9a8d81d09f9e3465045ddcd
迪南 林 <eamdfan@hotmail.com>
2019-09-07 22:28:45 +0800

    添加项目文件。

---
+++ ConsoleTempApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleTempApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
